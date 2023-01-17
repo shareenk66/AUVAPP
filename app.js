6d50a838abe025d02096e040493df95cc11e9db5
shareenk66 <shareenk66@gmail.com>
2023-01-18 00:23:47 +0530

    basic setup and election tables done

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const { Election,Question,Answer  } = require("./models");
+const path = require("path");
+
---
+++ models/answer.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Answer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Answer.init({
+    answerName: DataTypes.STRING,
+    voteCount: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Answer',
+  });
+  return Answer;
+};
